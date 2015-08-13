db1908457682df14757f8605362813ac0aa9ca37
zgy <346047221@qq.com>
2015-08-13 19:38:57 +0800

    AtyRest

---
+++ app/src/main/java/com/diting/zgy/forworkers/atys/AtyRest.java
@@ -0,0 +1,16 @@
+package com.diting.zgy.forworkers.atys;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.diting.zgy.forworkers.R;
+
+public class AtyRest extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aty_rest);
+    }
+
+}
